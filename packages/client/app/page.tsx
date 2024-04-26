4a63d811466017439bd3a4351bb02c2a4b771790
Afo <obaone01@gmail.com>
2024-04-25 22:14:46 -0700

    moved nextjs app to client folder under packages

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
-const AdminApp = dynamic(() => import("@/components/AdminApp"), { ssr: false });
+
+const AdminApp = dynamic(() => import("@/app/components/AdminApp"), {
+  ssr: false,
+});
 
 const Home: NextPage = () => <AdminApp />;
 
