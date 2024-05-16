284deeff73f72e6a1b6943cbcb1685496525b790
Afo <obaone01@gmail.com>
2024-05-16 14:35:08 -0700

    added next auth for handling user sessions

---
+++ packages/client/src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,3 @@
+import { handlers } from "@/modules/auth";
+
+export const { GET, POST } = handlers;
---
+++ packages/client/src/modules/auth.ts
@@ -0,0 +1,90 @@
+import { getCsrfToken } from "next-auth/react";
+import NextAuth, { DefaultSession } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { createAppClient, viemConnector } from "@farcaster/auth-kit";
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, and `getSession`
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string;
+      name: string;
+      image: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export const authConfig = {
+  relay: "https://relay.farcaster.xyz",
+  rpcUrl: "https://mainnet.optimism.io",
+  siweUri: "http://example.com/login",
+  domain: "example.com",
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      name: "Sign in with Farcaster",
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        message: {
+          label: "Message",
+          type: "text",
+          placeholder: "0x0",
+        },
+        signature: {
+          label: "Signature",
+          type: "text",
+          placeholder: "0x0",
+        },
+        // In a production app with a server, these should be fetched from
+        // your Farcaster data indexer rather than have them accepted as part
+        // of credentials.
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "0x0",
+        },
+        pfp: {
+          label: "Pfp",
+          type: "text",
+          placeholder: "0x0",
+        },
+      },
+      authorize: async (credentials) => {
+        try {
+          const csrfToken = await getCsrfToken();
+
+          const appClient = createAppClient({
+            ethereum: viemConnector(),
+          });
+
+          const verifyResponse = await appClient.verifySignInMessage({
+            message: credentials?.message as string,
+            signature: credentials?.signature as `0x${string}`,
+            domain: "example.com",
+            nonce: csrfToken,
+          });
+
+          const { success, fid } = verifyResponse;
+
+          if (!success) {
+            return null;
+          }
+
+          return {
+            id: fid.toString(),
+            name: credentials?.name as string,
+            image: credentials?.pfp as string,
+          };
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+});
