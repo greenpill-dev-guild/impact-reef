503d24fd37946382c7998493479269f6fa25bb2c
Afo <obaone01@gmail.com>
2024-05-16 14:38:24 -0700

    added mock data util removed uneeded styles function

---
+++ packages/client/src/utils/mockData.ts
@@ -0,0 +1,79 @@
+export const endorsments: { message: string; author: string; date: string }[] =
+  [
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "afo-wefa.eth",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "This section here should be fix height, and we should have max. character limit in the form.",
+      author: "chiali.eth",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "afo-wefa.eth",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "0x2aa64E6d80390F5C017F0313cB908051BE2FD35e",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "afo-wefa.eth",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "0x5d638a6E9089ea3140F97E85Cd1FC54bd6f0023e",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "afo-wefa.eth",
+      date: new Date().toISOString(),
+    },
+    {
+      message:
+        "Lorem ipsum dolor sit amet consectetur. Orci et molestie magna massa molestie. Ante iaculis laoreet et scelerisque augue sollicitudin condimentum lacinia est. Vulputate faucibus quam nunc porta posuere et vitae erat. Tellus sit lectus nulla sed auctor sapien at amet. Sed rutrum habitasse urna volutpat eget urna odio augue. Viverra facilisi orci maecenas enim sapien eget non. Tristique ac rhoncus metus etiam enim facilisis ac. Amet viverra ut tempus vel nisl lacus fringilla risus vitae adipiscing.",
+      author: "0x5d638a6E9089ea3140F97E85Cd1FC54bd6f0023e",
+      date: new Date().toISOString(),
+    },
+  ];
+
+export const metrics: { title: string; value: number }[] = [
+  {
+    title: "Number of OP Stack modules dependent on this contribution",
+    value: 0,
+  },
+  {
+    title: "Number of merged contributions to the OP Stack",
+    value: 3083091,
+  },
+  {
+    title: "Bytecode size reduction",
+    value: 0,
+  },
+  {
+    title: "Response time for RPC calls",
+    value: 482401,
+  },
+  {
+    title: "Gas costs reduction in contract creation",
+    value: 32,
+  },
+  {
+    title:
+      "Number of modules that were developed simultaneously without causing issues",
+    value: 0,
+  },
+];
