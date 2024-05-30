b09f46e5bea67e8afccea7bf0d580230fcb0f5fb
Afo <obaone01@gmail.com>
2024-05-30 00:58:37 -0700

    added million lint

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,8 +1,8 @@
+import MillionLint from '@million/lint';
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default process.env.ANALYZE === "true"
-  ? withBundleAnalyzer(nextConfig)
-  : nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(process.env.ANALYZE === "true" ? withBundleAnalyzer(nextConfig) : nextConfig);
