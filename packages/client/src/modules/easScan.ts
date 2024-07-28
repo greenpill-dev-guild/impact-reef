a33ccbbe919f916df40d5b18df14d9eadb1bd7d2
Afo <obaone01@gmail.com>
2024-07-28 14:11:33 -0700

    moved eas sepolia client to URQL

--- packages/client/src/modules/easScan.ts
+++
@@ -1,6 +0,0 @@
-import {Client, cacheExchange, fetchExchange} from 'urql';
-
-export const easScan = new Client({
-    url: 'https://sepolia.easscan.org/graphql',
-    exchanges: [cacheExchange, fetchExchange],
-});
--- packages/client/src/modules/urql.ts
+++ packages/client/src/modules/urql.ts
@@ -1,6 +1,12 @@
 import { Client, cacheExchange, fetchExchange } from "@urql/core";
 
 // TODO add getClient function based on chainID
+
+export const easSepoliaScan = new Client({
+  url: "https://sepolia.easscan.org/graphql",
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 export const easOptimismClient = new Client({
   url: "https://optimism.easscan.org/graphql",
   exchanges: [cacheExchange, fetchExchange],
