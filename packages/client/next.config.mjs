e402ec89fb579a5a25de74ccbe4adae24ecdb1ee
bitbeckers <code@bitbeckers.com>
2024-08-29 14:27:59 +0200

    feat(useProjects): updates useProject to use latest Agora API client and map objects

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,7 +1,24 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
+const AGORA_API_URL = process.env.AGORA_API_URL;
+const AGORA_API_KEY = process.env.AGORA_API_KEY;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    AGORA_API_URL: AGORA_API_URL,
+    AGORA_API_KEY: AGORA_API_KEY,
+  },
+  reactStrictMode: true,
+  // https://maxschmitt.me/posts/next-js-api-proxy
+  // https://nextjs.org/docs/pages/api-reference/next-config-js/rewrites
+  async rewrites() {
+    return [
+      {
+        source: '/api/agora/:path*',
+        destination: `${AGORA_API_URL}/:path*`,
+      },
+    ];
+  },
   async redirects() {
     return [
       {
@@ -16,7 +33,7 @@ const nextConfig = {
     return config
   },
   images: {
-    domains: ["picsum.photos"],
+    domains: ["picsum.photos", "storage.googleapis.com"],
   },
 };
 
--- packages/client/orval.config.ts
+++ packages/client/orval.config.ts
@@ -1,9 +1,21 @@
 import {defineConfig} from 'orval';
 
 export default defineConfig({
-    agora: {
+    "op-agora": {
         input: 'https://vote.optimism.io/api/v1/spec',
-        output: {target: './src/__generated__/api/agora.ts', baseUrl: "https://vote.optimism.io/api/v1/"},
+        output: {
+            mode: 'split',
+            target: './src/__generated__/api/agora.ts',
+            client: 'fetch',
+            baseUrl: '/api/agora/',
+            mock: true,
+            override: {
+                mutator: {
+                    path: './src/lib/custom-fetch.ts',
+                    name: 'customFetch',
+                },
+            },
+        },
         hooks: {
             afterAllFilesWrite: 'prettier --write',
         },
