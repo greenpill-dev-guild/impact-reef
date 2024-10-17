c3d217d69c1b3929b28126ba820d0564b0b004d0
Afo <afo@greenpill.builders>
2024-10-16 18:52:45 -0700

    updated request policy

--- packages/client/src/modules/urql.ts
+++ packages/client/src/modules/urql.ts
@@ -3,16 +3,14 @@ import { Client, cacheExchange, fetchExchange } from "@urql/core";
 export const easOptimismClient = new Client({
   url: "https://optimism.easscan.org/graphql",
   exchanges: [cacheExchange, fetchExchange],
-  fetchOptions: {
-    cache: "reload",
-  },
+  requestPolicy: "cache-and-network",
 });
 
 export const osoClient = new Client({
   url: "https://www.opensource.observer/api/v1/graphql",
   exchanges: [cacheExchange, fetchExchange],
+  requestPolicy: "cache-and-network",
   fetchOptions: {
-    cache: "reload",
     headers: {
       Authorization: `Bearer ${process.env.OPEN_SOURCE_OBSERVER_API_KEY}`,
     },
