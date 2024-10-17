21dd081c7cc191123d168c311f7cb611fd40140d
Afo <afo@greenpill.builders>
2024-10-16 19:03:34 -0700

    changed cache to network only

--- packages/client/src/modules/urql.ts
+++ packages/client/src/modules/urql.ts
@@ -3,13 +3,13 @@ import { Client, cacheExchange, fetchExchange } from "@urql/core";
 export const easOptimismClient = new Client({
   url: "https://optimism.easscan.org/graphql",
   exchanges: [cacheExchange, fetchExchange],
-  requestPolicy: "cache-and-network",
+  requestPolicy: "network-only",
 });
 
 export const osoClient = new Client({
   url: "https://www.opensource.observer/api/v1/graphql",
   exchanges: [cacheExchange, fetchExchange],
-  requestPolicy: "cache-and-network",
+  requestPolicy: "network-only",
   fetchOptions: {
     headers: {
       Authorization: `Bearer ${process.env.OPEN_SOURCE_OBSERVER_API_KEY}`,
