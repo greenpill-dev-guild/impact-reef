28db0f9839280f57f71c8c70cde1bd61dad67191
Afo <afo@greenpill.builders>
2024-10-16 18:33:07 -0700

    udated caching

--- packages/client/src/modules/urql.ts
+++ packages/client/src/modules/urql.ts
@@ -3,12 +3,16 @@ import { Client, cacheExchange, fetchExchange } from "@urql/core";
 export const easOptimismClient = new Client({
   url: "https://optimism.easscan.org/graphql",
   exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: {
+    cache: "reload",
+  },
 });
 
 export const osoClient = new Client({
   url: "https://www.opensource.observer/api/v1/graphql",
   exchanges: [cacheExchange, fetchExchange],
   fetchOptions: {
+    cache: "reload",
     headers: {
       Authorization: `Bearer ${process.env.OPEN_SOURCE_OBSERVER_API_KEY}`,
     },
