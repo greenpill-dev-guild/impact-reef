3d1ebf463efb8384075e2737851a8f629f27ba01
Afo <afo@greenpill.builders>
2024-10-16 19:12:53 -0700

    removed cache

--- packages/client/src/modules/urql.ts
+++ packages/client/src/modules/urql.ts
@@ -2,13 +2,13 @@ import { Client, cacheExchange, fetchExchange } from "@urql/core";
 
 export const easOptimismClient = new Client({
   url: "https://optimism.easscan.org/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [fetchExchange],
   requestPolicy: "network-only",
 });
 
 export const osoClient = new Client({
   url: "https://www.opensource.observer/api/v1/graphql",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [fetchExchange],
   requestPolicy: "network-only",
   fetchOptions: {
     headers: {
